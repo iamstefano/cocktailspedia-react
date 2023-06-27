2038f27cf8add958500ac5b7d5e392aa82bb09d4
Iamstefanoe <stefanoar3@gmail.com>
2023-06-27 16:48:07 +0200

    feature: productList&Item and edit prodSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import ProductSection from "./components/productSection";
+import ProductList from "./components/productList";
 
 function App() {
   const [productSection, setProductSection] = useState("");
 
   return (
-    <>
+    <div className="App">
       <h1>Cocktailspedia</h1>
 
-      <ProductSection
-        productSection={productSection}
-        setProductSection={setProductSection}
-      />
-    </>
+      {productSection ? (
+        <ProductSection
+          productSection={productSection}
+          setProductSection={setProductSection}
+        />
+      ) : (
+        <>
+          <ProductList name="rum" setProductSection={setProductSection} />
+          <ProductList name="vodka" setProductSection={setProductSection} />
+          <ProductList name="gin" setProductSection={setProductSection} />
+        </>
+      )}
+    </div>
   );
 }
 
---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { GET } from "../../utils/HTTP";
+import ProductItem from "../productItem";
+import "./index.css";
+
+const ProductList = ({ name, setProductSection }) => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + name)
+      .then((res) => res.json())
+      .then((data) => setListData(data.drinks));
+  }, []);
+
+  return (
+    <div className="ProductList">
+      <h2>{name}</h2>
+      <div className="ProductList__list">
+        {listData.map((drink) => (
+          <ProductItem
+            data={drink}
+            setProductSection={setProductSection}
+            key={drink.idDrink}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
