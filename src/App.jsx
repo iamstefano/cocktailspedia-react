f953be4ab111d4458cd83e1224b93ca405fd1e7f
Iamstefanoe <stefanoar3@gmail.com>
2023-06-29 14:35:59 +0200

    feat: contacts and App css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
+// import cocktailLogo from "./assets/cocktail.svg";
 import { useState } from "react";
-import "./App.css";
 import ProductSection from "./components/productSection";
 import ProductList from "./components/productList";
+import Contacts from "./components/contacts/Contacts";
+import "./App.css";
 
 function App() {
   const [productSection, setProductSection] = useState("");
 
+  const onRender = () => {
+    switch (productSection) {
+      case "contacts":
+        return <Contacts />;
+      case "":
+        return (
+          <>
+            <ProductList name="rum" setProductSection={setProductSection} />
+            <ProductList name="vodka" setProductSection={setProductSection} />
+            <ProductList name="gin" setProductSection={setProductSection} />
+          </>
+        );
+      default:
+        return (
+          <ProductSection
+            productSection={productSection}
+            setProductSection={setProductSection}
+          />
+        );
+    }
+  };
+
   return (
     <div className="App">
       <h1>Cocktailspedia</h1>
-
-      {productSection ? (
-        <ProductSection
-          productSection={productSection}
-          setProductSection={setProductSection}
-        />
-      ) : (
-        <>
-          <ProductList name="rum" setProductSection={setProductSection} />
-          <ProductList name="vodka" setProductSection={setProductSection} />
-          <ProductList name="gin" setProductSection={setProductSection} />
-        </>
-      )}
+      <ul>
+        <li onClick={() => setProductSection("")}>Home</li>
+        <li onClick={() => setProductSection("contacts")}>Contacts</li>
+      </ul>
+      {onRender()}
     </div>
   );
 }
---
+++ src/mock/auth.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    id: 1,
+    username: "stefano",
+    password: "stefano",
+  },
+  {
+    id: 2,
+    username: "tatata",
+    password: "tatata",
+  },
+];
